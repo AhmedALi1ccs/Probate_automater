d6557a7add8a0de4e5b68994877a95b0a102e630
AhmedALi <ahmedali27428@gmail.com>
2024-12-09 17:48:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Update system and install Playwright dependencies
 apt-get update && apt-get install -y \
     libnss3 libatk1.0-0 libx11-xcb1 libxcomposite1 \
     libxrandr2 libxdamage1 libpango1.0-0 libgbm1 \
     libasound2 fonts-liberation libfontconfig1
-playwright install
+
+# Force Playwright to install the browser binaries
+python -m playwright install

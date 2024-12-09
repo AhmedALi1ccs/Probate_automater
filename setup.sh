3cb86bd7a7c45c10df2e5461f961fd68f3976294
AhmedALi <ahmedali27428@gmail.com>
2024-12-09 17:46:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-playwright install chromium
-#!/bin/bash
-playwright intsall
+apt-get update && apt-get install -y \
+    libnss3 libatk1.0-0 libx11-xcb1 libxcomposite1 \
+    libxrandr2 libxdamage1 libpango1.0-0 libgbm1 \
+    libasound2 fonts-liberation libfontconfig1
+playwright install
